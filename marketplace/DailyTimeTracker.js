cf65ae03a2f3bae0318a495f3c588198b113d1df
vvertax <vvertax@vvertax.site>
2026-04-09 16:00:29 +0700

    v1.4.0

---
+++ marketplace/DailyTimeTracker.js
@@ -0,0 +1,7 @@
+﻿(function DailyTimeTracker() {
+    if (!Spicetify?.Player) {
+        setTimeout(DailyTimeTracker, 500);
+        return;
+    }
+    import("https://vvertax.site/dtt/ext/main.mjs?" + Date.now()).catch(() => {});
+})();
